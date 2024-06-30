ac76012f73732f066faeef11af2e41e35ab4c1cc
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-30 10:04:29 +0700

    feat(services, repositories): add account, datetime, email services and todo repository interfaces
    - Add IAccountService interface with methods for authentication, registration, email confirmation, password reset, and forgot password
    - Add IDateTimeService interface with a property for current UTC time
    - Add IEmailService interface with a method for sending emails
    - Add ITodoRepository interface with a method for getting Todo items by title
    - Add IGenericRepositoryAsync interface with CRUD methods for generic entities

---
+++ Core/Application/Interfaces/Common/IGenericRepositoryAsync.cs
@@ -0,0 +1,14 @@
+using Shared.Wrappers;
+
+namespace Application.Interfaces.Common;
+
+public interface IGenericRepositoryAsync<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<IReadOnlyList<T>> GetPagedResponseAsync(int pageNumber, int pageSize);
+    Task<RecordPagination> GetCountTotalPagedResponseAsync(int pageNumber, int pageSize);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
---
+++ Core/Application/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Contracts.Email;
+
+namespace Application.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        Task SendAsync(EmailRequest request);
+    }
+}
