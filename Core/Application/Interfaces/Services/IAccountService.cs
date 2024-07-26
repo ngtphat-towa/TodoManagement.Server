368236148b696267d15738cc224e62d534ea5ba9
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-26 09:36:45 +0700

    feat(application): add refresh token to account service & add documentation into user service

--- Core/Application/Interfaces/Services/IAccountService.cs
+++ Core/Application/Interfaces/Services/IAccountService.cs
@@ -8,10 +8,11 @@ public interface IAccountService
 {
     Task<Response<AuthenticationResponse>> AuthenticateAsync(AuthenticationRequest request, string ipAddress);
     Task<Response<string>> RegisterAsync(RegisterRequest request, string origin);
+    Task<Response<AuthenticationResponse>> RefreshTokenAsync(string token, string ipAddress);
     Task<Response<string>> ConfirmEmailAsync(string userId, string code);
     Task ForgotPassword(ForgotPasswordRequest model, string origin);
     Task<Response<string>> ResetPassword(ResetPasswordRequest model);
-    Task<Response<string>> LogoutAsync(string userId);
+    Task<Response<string>> LogoutAsync(string userId, string ipAddress);// aka RevokeToken
     Task<Response<List<string>>> GetUserRolesAsync(string userId);
     Task<Response<List<string>>> GetUserPermissionsAsync(string userId);
 }
