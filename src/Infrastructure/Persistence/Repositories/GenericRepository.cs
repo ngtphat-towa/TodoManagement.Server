0088ac6965212fe51c7e09dc08af22eedb941507
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-28 23:34:19 +0700

    feat(repository):  update GenericRepository implmenation

--- src/Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ src/Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -1,4 +1,6 @@
-﻿using Application.Interfaces.Common;
+﻿using System.Linq.Expressions;
+
+using Application.Interfaces.Common;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -8,14 +10,78 @@ using Shared.Wrappers;
 
 namespace Persistence.Repositories
 {
-
-    public class GenericRepository<T> : IGenericRepositoryAsync<T> where T : class
+    public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
     {
         private readonly ApplicationDbContext _dbContext;
 
-        public GenericRepository(ApplicationDbContext dbContext)
+        public GenericRepositoryAsync(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<Response<IEnumerable<T>>> GetByFilterAsync(DataFilter? dataFilter)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+
+            if (dataFilter != null && !string.IsNullOrWhiteSpace(dataFilter.PropertyName) && !string.IsNullOrWhiteSpace(dataFilter.PropertyValue))
+            {
+                var parameter = Expression.Parameter(typeof(T), "x");
+                var property = Expression.Property(parameter, dataFilter.PropertyName);
+                var value = ConvertToPropertyType(property.Type, dataFilter.PropertyValue);
+                var equals = Expression.Equal(property, Expression.Constant(value));
+                var lambda = Expression.Lambda<Func<T, bool>>(equals, parameter);
+                query = query.Where(lambda);
+            }
+
+            var result = await query.ToListAsync();
+            return Response<IEnumerable<T>>.Success(result);
+        }
+
+        public async Task<PagedResponse<IReadOnlyList<T>>> GetPagedResponseAsync(
+           PaginationFilter paginationFilter,
+           DataFilter? dataFilter,
+           DataSort? dataSort)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+
+            // Apply filter logic
+            if (dataFilter != null && !string.IsNullOrWhiteSpace(dataFilter.PropertyName) && !string.IsNullOrWhiteSpace(dataFilter.PropertyValue))
+            {
+                var parameter = Expression.Parameter(typeof(T), "x");
+                var property = Expression.Property(parameter, dataFilter.PropertyName);
+                var value = ConvertToPropertyType(property.Type, dataFilter.PropertyValue);
+                var equals = Expression.Equal(property, Expression.Constant(value));
+                var lambda = Expression.Lambda<Func<T, bool>>(equals, parameter);
+                query = query.Where(lambda);
+            }
+
+            // Apply sort logic
+            if (dataSort != null && !string.IsNullOrWhiteSpace(dataSort.SortBy))
+            {
+                var parameter = Expression.Parameter(typeof(T), "x");
+                var property = Expression.Property(parameter, dataSort.SortBy);
+                var lambda = Expression.Lambda<Func<T, object>>(Expression.Convert(property, typeof(object)), parameter);
+                query = dataSort.IsDescending ? query.OrderByDescending(lambda) : query.OrderBy(lambda);
+            }
+
+            // Get the total count of records
+            var totalRecords = await query.CountAsync();
+
+            // Apply pagination logic
+            var pagedQuery = query.Skip((paginationFilter.PageNumber - 1) * paginationFilter.PageSize)
+                                  .Take(paginationFilter.PageSize);
+
+            var items = await pagedQuery.ToListAsync();
+
+            // Calculate the total number of pages
+            var totalPages = (int)Math.Ceiling(totalRecords / (double)paginationFilter.PageSize);
+
+            return new PagedResponse<IReadOnlyList<T>>(items, paginationFilter.PageNumber, paginationFilter.PageSize, totalRecords, totalPages);
         }
 
         public async Task<T> AddAsync(T entity)
@@ -25,53 +91,59 @@ namespace Persistence.Repositories
             return entity;
         }
 
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
         public async Task DeleteAsync(T entity)
         {
             _dbContext.Set<T>().Remove(entity);
             await _dbContext.SaveChangesAsync();
         }
 
-        public async Task<IReadOnlyList<T>> GetAllAsync()
+        private object ConvertToPropertyType(Type propertyType, string value)
         {
-            return await _dbContext
-                 .Set<T>()
-                 .ToListAsync();
-        }
-
-        public virtual async Task<T?> GetByIdAsync(int id)
-        {
-            return await _dbContext.Set<T>().FindAsync(id);
-        }
-
-        public async Task<RecordPagination> GetCountTotalPagedResponseAsync(int pageNumber, int pageSize)
-        {
-            var totalRecords = await _dbContext.Set<T>().CountAsync();
-
-            var totalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
-
-            var paginationMetadata = new RecordPagination
+            if (propertyType == typeof(short) || propertyType == typeof(short?))
             {
-                TotalRecords = totalRecords,
-                TotalPages = totalPages
-            };
+                return short.Parse(value);
+            }
+            if (propertyType == typeof(int) || propertyType == typeof(int?))
+            {
+                return int.Parse(value);
+            }
+            if (propertyType == typeof(long) || propertyType == typeof(long?))
+            {
+                return long.Parse(value);
+            }
+            if (propertyType == typeof(float) || propertyType == typeof(float?))
+            {
+                return float.Parse(value);
+            }
+            if (propertyType == typeof(double) || propertyType == typeof(double?))
+            {
+                return double.Parse(value);
+            }
+            if (propertyType == typeof(decimal) || propertyType == typeof(decimal?))
+            {
+                return decimal.Parse(value);
+            }
+            if (propertyType == typeof(bool) || propertyType == typeof(bool?))
+            {
+                return bool.Parse(value);
+            }
+            if (propertyType == typeof(DateTime) || propertyType == typeof(DateTime?))
+            {
+                return DateTime.Parse(value);
+            }
+            if (propertyType == typeof(Guid) || propertyType == typeof(Guid?))
+            {
+                return Guid.Parse(value);
+            }
 
-            return paginationMetadata;
-        }
-
-
-        public async Task<IReadOnlyList<T>> GetPagedResponseAsync(int pageNumber, int pageSize)
-        {
-            return await _dbContext
-           .Set<T>()
-           .Skip((pageNumber - 1) * pageSize)
-           .Take(pageSize)
-           .ToListAsync();
-        }
-
-        public async Task UpdateAsync(T entity)
-        {
-            _dbContext.Entry(entity).State = EntityState.Modified;
-            await _dbContext.SaveChangesAsync();
+            // Default to string if no matching type found
+            return value;
         }
     }
 }
